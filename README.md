a4001f5e640cdb0b47234dd46b9726444600eca2
cachristopher <cory.a.christopher@gmail.com>
2020-12-15 09:10:23 -0700

    first push

---
+++ README.md
@@ -0,0 +1,7 @@
+# cli-s3-plugin
+
+This is a collection of s3 debug commands for Akkeris apps.
+
+## Installation
+
+`aka plugins:install s3`
---
+++ index.js
@@ -0,0 +1,42 @@
+const assert = require('assert')
+const common = require('../lib/common')
+
+async function cred_rotate (appkit, args) {
+  try {
+    let s3 = await common.find(appkit, args.app, args.bucket)
+    let data = null
+    if (s3.addon_service.name === 'akkeris-s3') {
+      data = await appkit.api.put(null, `/apps/${args.app}/addons/${s3.id}/actions/credentials`)
+    }
+    delete data.Plan
+    appkit.terminal.vtable(data)
+  } catch (err) {
+    appkit.terminal.error(err)
+  }
+}
+
+module.exports = {
+  init:function (appkit) {
+    let require_options = {
+      'app':{
+        'alias':'a',
+        'demand':false,
+        'string':true,
+        'description':'The app to act on.'
+      },
+      'bucket':{
+        'alias':'b',
+        'demand':false,
+        'string':true,
+        'description':'The name of the s3 addon to use'
+      }
+    }
+
+    appkit.args
+      .command('s3:credentials', 'rotate credentials', require_options, cred_rotate.bind(null, appkit))
+  },
+  update:function() {},
+  'group':'s3',
+  'help':'manage s3 addon',
+  'primary':false
+}
